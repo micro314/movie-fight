2aebe03f92094138e922b6751fd69bf8eefdb64c
Michael Rosati <rosati.michael@gmail.com>
2022-01-26 10:51:17 -0500

    Collect search term from user and pass to API on input change

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
-const fetchData = async () => {
+const fetchData = async (searchTerm) => {
     const response = await axios.get('http://www.omdbapi.com/', {
         params: {
             apikey: '20b79249',
-            s: 'avengers'
+            s: searchTerm
         }
     });
 
     console.log(response.data);
 }
+
+const input = document.querySelector('input');
+input.addEventListener('input', (event) => {
+    fetchData(event.target.value);
+});
