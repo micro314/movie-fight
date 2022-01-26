87e2cc0c36fcfb206c538a514d700cc440351e85
Michael Rosati <rosati.michael@gmail.com>
2022-01-26 09:48:43 -0500

    POC to demonstrate API connection to OMDB

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log('Hi there!');
+const fetchData = async () => {
+    const response = await axios.get('http://www.omdbapi.com/', {
+        params: {
+            apikey: '20b79249',
+            s: 'avengers'
+        }
+    });
+
+    console.log(response.data);
+}
