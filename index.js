675b6b0d28feb00c7a48094f12085067524e6de4
Michael Rosati <rosati.michael@gmail.com>
2022-01-26 16:39:07 -0500

    Parse movie details into numerics

--- index.js
+++ index.js
@@ -37,18 +37,20 @@ createAutoComplete({
     ...autoCompleteConfig,
     root: document.querySelector('#left-autocomplete'),
     onOptionSelect(movie) {
-        onMovieSelect(movie, document.querySelector('#left-summary'));
+        onMovieSelect(movie, document.querySelector('#left-summary'), 'left');
     },
 });
 createAutoComplete({
     ...autoCompleteConfig,
     root: document.querySelector('#right-autocomplete'),
     onOptionSelect(movie) {
-        onMovieSelect(movie, document.querySelector('#right-summary'));
+        onMovieSelect(movie, document.querySelector('#right-summary'), 'right');
     },
 });
 
-const onMovieSelect = async (movie, summaryElement) => {
+let leftMovie;
+let rightMovie;
+const onMovieSelect = async (movie, summaryElement, side) => {
     document.querySelector('.tutorial').classList.add('is-hidden');
     const response = await axios.get(omdbData.url, {
         params: {
@@ -58,9 +60,38 @@ const onMovieSelect = async (movie, summaryElement) => {
     })
 
     summaryElement.innerHTML = movieTemplate(response.data);
+
+    if (side === 'left') {
+        leftMovie = response.data;
+    } else {
+        rightMovie = response.data;
+    }
+
+    if (leftMovie && rightMovie) {
+        runComparison();
+    }
+}
+
+const runComparison = () => {
+
 }
 
 const movieTemplate = (movieDetail) => {
+    const dollars = parseInt(movieDetail.BoxOffice.replace(/\$/g, '').replace(/,/g, ''));
+    const metascore = parseInt(movieDetail.Metascore);
+    const imdbRating = parseFloat(movieDetail.imdbRating);
+    const imdbVotes = parseInt(movieDetail.imdbVotes.replace(/,/g, ''));
+    
+    const awards = movieDetail.Awards.split(' ').reduce((prev, word) => {
+        const value = parseInt(word);
+        if (isNaN(value)) {
+            return prev;
+        } else {
+            return prev + value;;
+        }
+    }, 0);
+    console.log(awards);
+    
     return `
         <article class="media">
             <figure class="media-left">
