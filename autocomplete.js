3c56951d7de9f2025755fd97f99b9e8c42cf67e1
Michael Rosati <rosati.michael@gmail.com>
2022-01-26 14:40:02 -0500

    Make multiple autocomplete widgets displayable

---
+++ autocomplete.js
@@ -0,0 +1,56 @@
+const createAutoComplete = ({ root }) => {
+    root.innerHTML = `
+    <label><b>Search for a Movie</b></label>
+    <input class="input" />
+    <div class="dropdown">
+    <div class="dropdown-menu">
+        <div class="dropdown-content results"></div>
+    </div>
+    </div>
+    <div id="target"></div>
+`;
+
+    const input = root.querySelector('.input');
+    const dropdown = root.querySelector('.dropdown');
+    const resultsWrapper = root.querySelector('.results');
+
+    const showDropdown = () => {
+        dropdown.classList.add('is-active');
+    }
+    const hideDropdown = () => {
+        dropdown.classList.remove('is-active');
+    }
+
+    const onInput = async event => {
+        const movies = await fetchData(event.target.value);
+        if (!movies.length) {
+            hideDropdown();
+            return;
+        }
+
+        resultsWrapper.innerHTML = '';
+        showDropdown();
+        for (let movie of movies) {
+            const option = document.createElement('a');
+            option.classList.add('dropdown-item');
+            const imgSrc = movie.Poster === 'N/A' ? '' : movie.Poster;
+            option.innerHTML = `
+            <img src="${imgSrc}" />
+            ${movie.Title}
+        `;
+            option.addEventListener('click', () => {
+                hideDropdown();
+                input.value = movie.Title;
+                onMovieSelect(movie);
+            });
+            resultsWrapper.appendChild(option);
+        }
+    }
+
+    input.addEventListener('input', debounce(onInput, 500));
+    document.addEventListener('click', event => {
+        if (!root.contains(event.target)) {
+            hideDropdown();
+        }
+    });
+};
--- index.js
+++ index.js
@@ -17,60 +17,14 @@ const fetchData = async (searchTerm) => {
     return response.data.Search;
 }
 
-const root = document.querySelector('.autocomplete');
-root.innerHTML = `
-    <label><b>Search for a Movie</b></label>
-    <input class="input" />
-    <div class="dropdown">
-    <div class="dropdown-menu">
-        <div class="dropdown-content results"></div>
-    </div>
-    </div>
-    <div id="target"></div>
-`;
-
-const input = document.querySelector('.input');
-const dropdown = document.querySelector('.dropdown');
-const resultsWrapper = document.querySelector('.results');
-
-const showDropdown = () => {
-    dropdown.classList.add('is-active');
-}
-const hideDropdown = () => {
-    dropdown.classList.remove('is-active');
-}
-
-const onInput = async event => {
-    const movies = await fetchData(event.target.value);
-    if (!movies.length) {
-        hideDropdown();
-        return;
-    }
-
-    resultsWrapper.innerHTML = '';
-    showDropdown();
-    for (let movie of movies) {
-        const option = document.createElement('a');
-        option.classList.add('dropdown-item');
-        const imgSrc = movie.Poster === 'N/A' ? '' : movie.Poster;
-        option.innerHTML = `
-            <img src="${imgSrc}" />
-            ${movie.Title}
-        `;
-        option.addEventListener('click', () => {
-            hideDropdown();
-            input.value = movie.Title;
-            onMovieSelect(movie);
-        });
-        resultsWrapper.appendChild(option);
-    }
-}
-
-input.addEventListener('input', debounce(onInput, 500));
-document.addEventListener('click', event => {
-    if (!root.contains(event.target)) {
-        hideDropdown();
-    }
+createAutoComplete({
+    root: document.querySelector('.autocomplete')
+});
+createAutoComplete({
+    root: document.querySelector('.autocomplete-2')
+});
+createAutoComplete({
+    root: document.querySelector('.autocomplete-3')
 });
 
 const onMovieSelect = async movie => {
